2a250cc349b742647727466c36ad04d8d2c147fa
MinHsuanyu <minhsuanyu@gmail.com>
2022-08-30 01:48:18 +0800

    unfinish

---
+++ models/residual_block.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, stride=1, shortcut=None):
+        super(ResidualBlock, self).__init__()
+        self.left = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, 3, stride, 1, bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.ReLU(),
+            nn.Conv2d(out_channel, out_channel, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channel)
+        )
+        self.right = shortcut
+    def forward(self, x):
+        out = self.left(x)
+        residual = x if self.right is None else self.right(x)
+        out = out + residual
+        out = F.relu(out)
+        return out
+
+model = ResidualBlock(128, 64)
+print(model)
+

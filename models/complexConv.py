08cb351a75efad4bc8f95d05a8a865a929bfa81f
MinHsuanyu <minhsuanyu@gmail.com>
2022-08-31 19:59:00 +0800

    unfinished

--- models/complexConv.py
+++ models/complexConv.py
@@ -7,24 +7,31 @@ import torchvision.models as models
 #Complex Conv 2 times
 class ComplexCon2D(nn.Module):
     def __init__(self, in_channel, out_channel):
-        super(ComplexCon, self).__init__()
-        self.Conv_real_1 = nn.Conv2d(in_channel//2, out_channel//2, 3, 1, 1, bias=False)
-        self.Conv_image_1 = nn.Conv2d(in_channel//2, out_channel//2, 3, 1, 1, bias=False)
+        super(ComplexCon2D, self).__init__()
+        self.Conv_real_1 = nn.Conv2d(in_channel//2, out_channel//2, 3, 1, 1)
+        self.Conv_image_1 = nn.Conv2d(in_channel//2, out_channel//2, 3, 1, 1)
         self.Conv_real_2 = nn.Conv2d(out_channel//2, out_channel//2, 3, 1, 1, bias=False)
         self.Conv_image_2 = nn.Conv2d(out_channel//2, out_channel//2, 3, 1, 1, bias=False)
     def forward(self, x):
-        print("x shape: ", x.shape)
         x_real, x_image = torch.chunk(x, 2, dim=1)
-        print(x_real.shape, x_image.shape)
         x1_real = self.Conv_real_1(x_real) - self.Conv_image_1(x_image)
-        print(x1_real.shape)
         x1_image = self.Conv_image_1(x_real) + self.Conv_real_1(x_image)
-        print(x1_image.shape)
         x2_real = self.Conv_real_2(x1_real) - self.Conv_image_2(x1_image)
-        print(x2_real.shape)
         x2_image = self.Conv_image_2(x1_real) + self.Conv_real_2(x1_image)
-        print(x2_image.shape)
         return torch.cat((x2_real, x2_image), dim=1)
 
 class ComplexCon1D(nn.Module):
-    
+    def __init__(self, in_channel, out_channel):
+        super(ComplexCon1D, self).__init__()
+        self.Conv_real_1 = nn.Conv1d(in_channel//2, out_channel//2, 3, 1, 1)
+        self.Conv_image_1 = nn.Conv1d(in_channel//2, out_channel//2, 3, 1, 1)
+        self.Conv_real_2 = nn.Conv1d(out_channel//2, out_channel//2, 3, 1, 1, bias=False)
+        self.Conv_image_2 = nn.Conv1d(out_channel//2, out_channel//2, 3, 1, 1, bias=False)
+    def forward(self, x):
+        x_real, x_image = torch.chunk(x, 2, dim=1)
+        print(x_real.dtype)
+        x1_real = self.Conv_real_1(x_real) - self.Conv_image_1(x_image)
+        x1_image = self.Conv_image_1(x_real) + self.Conv_real_1(x_image)
+        x2_real = self.Conv_real_2(x1_real) - self.Conv_image_2(x1_image)
+        x2_image = self.Conv_image_2(x1_real) + self.Conv_real_2(x1_image)
+        return torch.cat((x2_real, x2_image), dim=1)
--- models/test.py
+++ models/test.py
@@ -3,11 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 import torchsummary
+import numpy as np
 
 import complexConv
 
-model = complexConv.ComplexCon(2, 16).cuda()
-print(model)
-torchsummary.summary(model, (2, 1, 2048))
+device = "cpu"
 
 
+model = complexConv.ComplexCon1D(2, 16).double()
+x = torch.tensor(np.ndarray((1, 2, 2048))).to(device)
+y = model(x)
+print(y)
